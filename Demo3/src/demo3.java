fea4fa44d6b1d732b956ecc39e409a4b67bb39e2
zhonglihua <lihua.zhong@dragonfow.com>
2013-04-01 15:10:39 +0800

    ddd

--- Demo3/src/demo3.java
+++ Demo3/src/demo3.java
@@ -7,6 +7,7 @@ public class demo3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hehe 1111111111");
+	System.out.println("hehe 22222222222");
 	}
 
 }
